e2636c4356b082d17c2a3b1c5d6cec0d7934dd95
YURI F S FREITAS <iruytj@gmail.com>
2021-05-13 19:45:31 -0300

    PRIMEIRO PASSO
    ENVIANDO 1

---
+++ index.bat
@@ -0,0 +1,4 @@
+"G:\SOFTCOD\PesquisaCorona"  /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+File.OpenFile  "G:\SOFTCOD\PesquisaCorona" /e: "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE"
+
+pause()
